b01c034e8177ddfbf4d55f688f51d381b94f5bcd
Phixyn <phixyn@gmail.com>
2016-07-15 02:18:59 +0100

    feat(socket-io): implement a very rustic timestamp but it's 2am pls don't judge ;_;

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Timestamp bla
+function timestamp() {
+    var now = new Date();
+    var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+    // If hours, minutes or seconds are less than 10, add a zero in front
+    for (var i = 0; i < 3; i++) {
+        if (time[i] < 10) {
+            time[i] = "0" + time[i];
+        }
+    }
+
+    // Return the formatted string
+    return '[' + time.join(":") + "]";
+}
+
 app.get('/', function(req, res) {
     // res.send('<h1>Hello world!</h1>');
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log('[INFO] A user has connected!');
+    console.log(timestamp() + '[INFO] A user has connected!');
 
     socket.on('disconnect', function() {
-        console.log('[INFO] A user has disconnected.');
+        console.log(timestamp() + '[INFO] A user has disconnected.');
     });
 
     socket.on('chat_message', function(data) {
-        console.log('[CHAT] ' + data.uname + ': ' + data.msg);
+        console.log(timestamp() + '[CHAT] ' + data.uname + ': ' + data.msg);
         io.emit('chat_message', data);
     });
 });
