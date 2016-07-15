b8cbe2fe7a69da0923a9f63e90289a754fafc679
Phixyn <phixyn@gmail.com>
2016-07-15 01:51:16 +0100

    feat(socket-io): implement usernames

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-    console.log('[INFO] A user connected');
+    console.log('[INFO] A user has connected!');
 
     socket.on('disconnect', function() {
-        console.log('[INFO] A user disconnected');
+        console.log('[INFO] A user has disconnected.');
     });
 
-    socket.on('chat message', function(msg) {
-        console.log('[CHAT] User: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat_message', function(data) {
+        console.log('[CHAT] ' + data.uname + ': ' + data.msg);
+        io.emit('chat_message', data);
     });
 });
 
