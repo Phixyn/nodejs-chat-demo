2b4fd0c1a8797a6951c7749c54a7d57f67343c39
Phixyn <phixyn@gmail.com>
2016-07-18 22:03:44 +0100

    refactor: move timestamp code to a utils script

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express'),
+	app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
 
-// Timestamp bla
-function timestamp() {
-    var now = new Date();
-    var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
-
-    // If hours, minutes or seconds are less than 10, add a zero in front
-    for (var i = 0; i < 3; i++) {
-        if (time[i] < 10) {
-            time[i] = "0" + time[i];
-        }
-    }
-
-    // Return the formatted string
-    return '[' + time.join(":") + "]";
-}
+var util = require('./lib/util.js');
 
 app.get('/', function(req, res) {
-    // res.send('<h1>Hello world!</h1>');
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log(timestamp() + '[INFO] A user has connected!');
+    console.log(util.timestamp() + '[INFO] A user has connected!');
 
     socket.on('disconnect', function() {
-        console.log(timestamp() + '[INFO] A user has disconnected.');
+        console.log(util.timestamp() + '[INFO] A user has disconnected.');
     });
 
     socket.on('chat_message', function(data) {
-        console.log(timestamp() + '[CHAT] ' + data.uname + ': ' + data.msg);
+        console.log(util.timestamp() + '[CHAT] ' + data.uname + ': ' + data.msg);
         io.emit('chat_message', data);
     });
 });
 
+// Serve static content to clients: /public folder
+app.use(express.static(__dirname + '/public'));
+
 http.listen(3000, function() {
-    console.log('Listening on *:3000\n');
+    console.log('Listening on port 3000\n');
 });
+
---
+++ public/js/util.js
@@ -0,0 +1,16 @@
+function timestamp() {
+	// Return a pretty timestamp
+	var now = new Date();
+	var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+	// If hours, minutes or seconds are less than 10, add a zero in front
+	for (var i = 0; i < 3; i++) {
+		if (time[i] < 10) {
+			time[i] = "0" + time[i];
+		}
+	}
+
+	// Return the formatted string
+	return '[' + time.join(":") + "]";
+}
+
