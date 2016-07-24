673fa4d8ba10b4addc056692c1392bf00c4613b5
Phixyn <phixyn@gmail.com>
2016-07-24 20:03:14 +0100

    feat: display chat and log messages when users join/leave the chat

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ io.on('connection', function(socket) {
 			userList.push(data);
 			// Send an event to connected clients with list of connected users
 			io.emit('users_list', userList);
+			// Send user join event
+			io.emit('user_join', socket.username);
+			console.log(util.timestamp() + '[INFO] ' + socket.username +
+				' has joined the channel.');
 		}
 	});
 
@@ -35,12 +39,14 @@ io.on('connection', function(socket) {
     socket.on('disconnect', function() {
         console.log(util.timestamp() + '[INFO] A user has disconnected.');
 		if (!socket.username) return false;
-		console.log(util.timestamp() + '[INFO] ' + socket.username +
-			' has left the room.');
 		// Remove username from user list
 		userList.splice(userList.indexOf(socket.username), 1);
 		// Send updated user list to connected clients
 		io.emit('users_list', userList);
+		// Send user leave event
+		io.emit('user_leave', socket.username);
+		console.log(util.timestamp() + '[INFO] ' + socket.username +
+			' has left the channel.');
     });
 });
 
